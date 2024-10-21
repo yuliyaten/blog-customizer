70e006ccb37189e4ac5513693bba0e8f037fa35e
Yuliya Ten <juliia.ten@gmail.com>
2024-10-21 22:47:50 +0900

    Внесены изменения по комментариям

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -3,95 +3,86 @@ import styles from './ArticleParamsForm.module.scss';
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 import { RadioGroup } from 'src/ui/radio-group';
-import { fontFamilyOptions, fontSizeOptions, fontColors, backgroundColors, contentWidthArr, OptionType } from 'src/constants/articleProps';
+import { backgroundColors, contentWidthArr, defaultArticleState, fontColors, fontFamilyOptions, fontSizeOptions, OptionType } from 'src/constants/articleProps';
 import { Select } from 'src/ui/select';
 import { Separator } from 'src/ui/separator';
 import { useOutsideClickClose } from 'src/ui/select/hooks/useOutsideClickClose';
 import { useState, useRef } from 'react';
 
 export const ArticleParamsForm = ({ onApply, onReset }: { onApply: (state: any) => void; onReset: () => void }) => {
-	const [isOpen, setState] = useState(false);
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
 	const rootRef = useRef<HTMLDivElement>(null);
-	const toogle = () => { setState((prevState) => !prevState) };
+	const toogle = () => { setIsMenuOpen((prevState) => !prevState) };
 
-	const [selectedFontFamily, FontFamily] = useState<OptionType>(fontFamilyOptions[0]);
-	const [selectedFontSize, FontSize] = useState<OptionType>(fontSizeOptions[0]);
-	const [selectedFontColor, FontColor] = useState<OptionType>(fontColors[0]);
-	const [selectedBackgroundColor, BackgroundColor] = useState<OptionType>(backgroundColors[0]);
-	const [selectedContentWidth, ContentWidth] = useState<OptionType>(contentWidthArr[0]);
+ const [articleState, setArticleState] = useState(defaultArticleState);
+
+	const selectChange = (key: keyof typeof articleState, value: OptionType) => {
+		setArticleState((prevState) => ({ ...prevState, [key]: value }));
+};
 
 	const submit = (e: React.FormEvent) => {
 		e.preventDefault();
 		onApply({
-				fontFamily: selectedFontFamily.value,
-				fontSize: selectedFontSize.value,
-				fontColor: selectedFontColor.value,
-				backgroundColor: selectedBackgroundColor.value,
-				contentWidth: selectedContentWidth.value,
+				fontFamily: articleState.fontFamilyOption.value,
+				fontSize: articleState.fontSizeOption.value,
+				fontColor: articleState.fontColor.value,
+				backgroundColor: articleState.backgroundColor.value,
+				contentWidth: articleState.contentWidth.value,
 		});
 	};
 
 	const reset = () => {
-		FontFamily(fontFamilyOptions[0]);
-		FontSize(fontSizeOptions[0]);
-		FontColor(fontColors[0]);
-		BackgroundColor(backgroundColors[0]);
-		ContentWidth(contentWidthArr[0]);
+		setArticleState(defaultArticleState);
 		onReset();
 };
 
 	useOutsideClickClose({
-		isOpen,
+		isOpen: isMenuOpen,
 		rootRef,
-		onChange: setState,
+		onChange: setIsMenuOpen,
 	});
 
 	return (
 		<>
-			<ArrowButton isOpen={isOpen} onClick={toogle} />
+			<ArrowButton isOpen={isMenuOpen} onClick={toogle} />
 			<aside
 				ref={rootRef}
-				className={clsx(styles.container, { [styles.container_open]: isOpen })}>
+				className={clsx(styles.container, { [styles.container_open]: isMenuOpen })}>
 				<form className={styles.form} onSubmit={submit}>
 					<Select
 						options={fontFamilyOptions}
 						placeholder = "Выберите шрифт"
-						selected={selectedFontFamily}
-						onChange={FontFamily}
-						onClose={() => setState(false)}
+						selected={articleState.fontFamilyOption}
+						onChange={(value) => selectChange('fontFamilyOption', value)}
 						title='Шрифт'
 					/>
 					<RadioGroup
 						name='font-size'
 						options={fontSizeOptions}
-						selected={selectedFontSize}
-						onChange={FontSize}
+						selected={articleState.fontSizeOption}
+						onChange={(value) => selectChange('fontSizeOption', value)}
 						title='Размер шрифта'
-						
 					/>
 					<Select
 						options={fontColors}
 						placeholder="Выберите цвет шрифта"
-						selected={selectedFontColor}
-						onChange={FontColor}
-						onClose={() => setState(false)}
+						selected={articleState.fontColor}
+						onChange={(value) => selectChange('fontColor', value)}
 						title='Цвет шрифта'
 					/>
 					<Separator />
 					<Select
 						options={backgroundColors}
 						placeholder="Выберите цвет фона"
-						selected={selectedBackgroundColor}
-						onChange={BackgroundColor}
-						onClose={() => setState(false)}
+						selected={articleState.backgroundColor}
+						onChange={(value) => selectChange('backgroundColor', value)}
 						title='Цвет фона'
 					/>
 					<Select
 						options={contentWidthArr}
 						placeholder="Выберите ширину контента"
-						selected={selectedContentWidth}
-						onChange={ContentWidth}
-						onClose={() => setState(false)}
+						selected={articleState.contentWidth}
+						onChange={(value) => selectChange('contentWidth', value)}
 						title='Ширина контента'
 					/>
 					<div className={styles.bottomContainer}>
